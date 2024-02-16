2121f904e6ccc15e441cdd678efad3df5eda56f5
tuyen1123 <tuyen1122@4nrkx7.onmicrosoft.com>
2024-02-16 14:07:37 +0700

    push code lan 1

---
+++ ChuoiServer.java
@@ -0,0 +1,26 @@
+
+
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ChuoiServer {
+  public static void main(String[] args) throws MalformedURLException, AlreadyBoundException {
+	System.out.println("Server dang khoi dong");
+	
+	int port=9988;
+	try {
+		LocateRegistry.createRegistry(port);
+		ChuoiImpl ch= new ChuoiImpl();
+		UnicastRemoteObject.exportObject(ch,port);
+		Naming.bind("rmi://localhost:9988/Mychuoi", ch);
+		System.out.println("Server da ket noi");
+	} catch (RemoteException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+}
